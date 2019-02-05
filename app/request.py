acadd2baada23ed1ee08312fa13abfc1d7098ba7
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-05 11:39:09 +0300

    Access our app configuration objects

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import sources
+
+Sources= sources.Sources
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the sources base url
+sources_url = app.config['SOURCES_API_BASE_URL']
