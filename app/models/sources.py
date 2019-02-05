322eb240689f4104fa0ae59c4af16f936e9a0580
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-05 11:31:19 +0300

    Created the sources class

---
+++ app/models/sources.py
@@ -0,0 +1,10 @@
+class Sources:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,name,description,url,category):
+        self.name =name
+        self.description = description
+        self.url = url
+        self.category = category
