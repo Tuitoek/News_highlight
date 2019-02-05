06df494bd88c706862caa7cf8285fd76002564fb
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-05 11:43:30 +0300

    Function that takes sources category as an argument

--- app/request.py
+++ app/request.py
@@ -9,3 +9,22 @@ api_key = app.config['NEWS_API_KEY']
 
 #Getting the sources base url
 sources_url = app.config['SOURCES_API_BASE_URL']
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_results(sources_results_list)
+
+
+    return sources_results
