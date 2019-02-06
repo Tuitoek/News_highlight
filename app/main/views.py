94f6cafde29342911f2294834467ea5ee8ac29ee
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-06 12:17:41 +0300

    Add main file

---
+++ app/main/views.py
@@ -0,0 +1,35 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_sources,get_articles
+from ..models import Sources,Articles
+
+#Views
+@main.route('/')
+def source():
+
+    '''
+    View root page function that returns the source page and its data
+    '''
+    #Getting general news
+    general_news = get_sources('general')
+    print(general_news)
+    #getting technology news
+    technology_news = get_sources('technology')
+    #getting sport_news
+    sport_news = get_sources('sports')
+    #getting business news
+    business_news = get_sources('business')
+    #getting science news
+    science_news = get_sources('science')
+
+    title = 'Home -Let us get to the briefs'
+    return render_template('source.html', title = title,general=general_news,technology=technology_news,sports=sport_news,business=business_news,science=science_news)
+
+@main.route('/')
+def article():
+    '''
+    Returns the article page and its data
+    '''
+    article=get_articles
+    title ='Articles'
+    return render_template('article.html',title=title,article = get_articles)
