e987183da538119c134f95d2cf3abfd9c7db5b1a
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-06 12:15:15 +0300

    Add config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+class Config:
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    ARTICLE_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
